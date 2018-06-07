3de824c3ffde7a489548e7c0d1cf3520555bdb97
a-guerrero <adrian@foostudio.mx>
2018-06-07 13:01:06 -0500

    Light and shadow test

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ const updatePlaneSize = (plane, camera, scale = 1) => {
  * @param {number} x
  * @param {number} phase
  */
-const cosWave = (y1, y2, x, phase = 0) => {
+const cosWave = (y1, y2, x, phase = 1) => {
     const amplitude = (y2 - y1) / 2
     return y1 + amplitude + amplitude * Math.cos(x * phase)
 }
@@ -99,59 +99,63 @@ body.appendChild(renderer.domElement)
 
 // HEMISPHERE LIGHT
 ////////////////////////////////////////////////////////////////////////////////
-const hemiLight = new THREE.HemisphereLight(0XF4F2E4, 0X322F2A, 1)
+const hemiLight = new THREE.HemisphereLight(0XF4F2E4, 0X322F2A, 0.5)
 hemiLight.position.y = -6
 hemiLight.position.z = 7
-// scene.add(hemiLight)
+scene.add(hemiLight)
 
 // DIRECTIONAL LIGHT
 ////////////////////////////////////////////////////////////////////////////////
 const pointLight = new THREE.PointLight(0XF4F2E4, 1, 100)
 pointLight.castShadow = true
+pointLight.shadow.mapSize.width = 2048
+pointLight.shadow.mapSize.height = 2048
+pointLight.shadow.camera.near = 0.1
+pointLight.shadow.camera.far = 15
 pointLight.position.y = -3
-pointLight.position.z = 7
-pointLight.shadowCameraVisible = true
-console.log(pointLight.shadowCameraVisible)
+pointLight.position.z = 8
 scene.add(pointLight)
+
 const directionalLightHelper = new THREE.PointLightHelper(pointLight, 0.1, 0xFFFF00)
 scene.add(directionalLightHelper)
 
 // BACKGROUND
 ////////////////////////////////////////////////////////////////////////////////
-const backgroundGeom = new THREE.PlaneGeometry(4, 4)
+const backgroundGeom = new THREE.BoxGeometry(4, 4, 0.01)
 const backgroundMat = new THREE.MeshPhongMaterial({
-    color: 0X2B2B2F,
-    specular: 0X2B2B2F,
-    shininess: 20,
+    color: 0XFF0000,
+    specular: 0X00FFAE,
+    shininess: 2,
     morphTargets: true,
     vertexColors: THREE.FaceColors,
     flatShading: true,
 })
 const background = new THREE.Mesh(backgroundGeom, backgroundMat)
 background.receiveShadow = true
-background.castShadow = true
-background.z = -4
+background.z = -1
+updatePlaneSize(background, camera, 0.9)
+pointLight.add(background)
 scene.add(background)
 
 // PLANE
 ////////////////////////////////////////////////////////////////////////////////
-const geometry = new THREE.PlaneGeometry(1, 1)
+const geometry = new THREE.BoxGeometry(1, 1, 0.01)
 const material = new THREE.MeshPhongMaterial({
     color: 0X999999,
     specular: 0X999999,
-    shininess: 20,
+    shininess: 0,
     morphTargets: true,
     vertexColors: THREE.FaceColors,
     flatShading: true,
 })
 
 const plane = new THREE.Mesh(geometry, material)
+const maxPlaneRotation = degToRad(45)
 plane.castShadow = true
-plane.receiveShadow = true
-plane.position.z = 2
-// plane.rotation.x = degToRad(10)
-// plane.rotation.y = degToRad(-10)
-// updatePlaneSize(plane, camera, 0.9)
+plane.position.z = 1
+plane.rotation.x = maxPlaneRotation
+plane.rotation.y = -maxPlaneRotation
+pointLight.add(plane)
 scene.add(plane)
 
 // ANIMATION
@@ -159,9 +163,12 @@ scene.add(plane)
 const then = Date.now()
 const animate = () => {
     const now = Date.now()
-    const delta = now - then
+    const delta = (now - then) * 0.0008
     requestAnimationFrame(animate)
-    pointLight.position.y = cosWave(3, -3, delta, 0.00025)
+    pointLight.position.x = cosWave(-5, 5, delta)
+    pointLight.position.y = cosWave(5, -5, delta)
+    plane.rotation.x = cosWave(-maxPlaneRotation, maxPlaneRotation, delta)
+    plane.rotation.y = cosWave(-maxPlaneRotation, maxPlaneRotation, delta)
     renderer.render(scene, camera)
 }
 
